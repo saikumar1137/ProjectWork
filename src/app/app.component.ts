19528ee088dfb099a831a5addd7e061f1e06e08c
Sai_Kumar Pulugu <saikumarpspk1919@gmail.com>
2021-11-13 21:51:22 +0530

    changes updated

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,6 @@
 import { Component,OnInit } from '@angular/core';
 import { FormBuilder,FormGroup } from '@angular/forms';
-
+import  *  as  data  from  '../assets/data.json';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
@@ -14,7 +14,8 @@ export class AppComponent implements OnInit {
   makedis2:boolean=false;
   makedis3:boolean=false;
   makedis4:boolean=false;
-
+valuearr:any=[];
+namesarr:any=[];
 constructor(private fb: FormBuilder){
   this.form = this.fb.group({
     Qty1:[''],
@@ -24,8 +25,11 @@ constructor(private fb: FormBuilder){
   })
 }
 ngOnInit():void{
-console.log(this.form.value.Qty1);
 
+this.namesarr.push(data[0].p_name,data[1].p_name,data[1].p_name,data[2].p_name);
+console.log(this.namesarr);
+this.valuearr.push(data[0].p_cost,data[1].p_cost,data[2].p_cost,data[3].p_cost);
+console.log(this.valuearr);
 
 }
 result(){
